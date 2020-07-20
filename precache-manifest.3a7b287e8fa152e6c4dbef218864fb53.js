c3c937290692529c7c1dec4952597cfe97709166
Biplav Sharma <nifty@Biplavs-MacBook-Pro.local>
2020-07-21 01:34:43 +0545

    Updates

---
+++ precache-manifest.3a7b287e8fa152e6c4dbef218864fb53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03962c95de222edfe02bb0cb7ec00212",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "91568ea3b3a5bcb12e9f",
+    "url": "/QuizApp/static/js/2.22b4a464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizApp/static/js/2.22b4a464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97906d004818d0578554",
+    "url": "/QuizApp/static/js/main.b43f0d9e.chunk.js"
+  },
+  {
+    "revision": "30c85ce07bf1df642fe0",
+    "url": "/QuizApp/static/js/runtime-main.89d0d1ff.js"
+  }
+]);
